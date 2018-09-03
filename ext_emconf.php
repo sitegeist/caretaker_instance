25553348696d86d98510de65a357045927139631
Simon Praetorius <praetorius@sitegeist.de>
2018-09-03 13:26:30 +0200

    [TASK] Prepare for TYPO3 9

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,12 +18,12 @@ $EM_CONF[$_EXTKEY] = array (
   'clearCacheOnLoad' => 0,
   'lockType' => '',
   'author_company' => '',
-  'version' => '0.8.1',
+  'version' => '0.8.1-dev',
   'constraints' => 
   array (
     'depends' =>
     array (
-      'typo3' => '6.2.0-8.7.99',
+      'typo3' => '6.2.0-9.5.99',
     ),
     'conflicts' =>
     array (
